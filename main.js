b635a79fe5e7a0c8789703bfb9e07be6379368ea
Ramonpol10 <ramonzinevil369@gmai.com>
2025-04-15 22:03:13 -0300

    Finalização do código correto

--- main.js
+++ main.js
@@ -1,17 +1,17 @@
 // Implementa acesso para a tela de login
-function acessar (){
+function acessar(){
     let loginEmail = document.getElementById('loginEmail').value;
     let loginSenha = document.getElementById('loginSenha').value;
+    // Implementa estrutura que testa se todos os campos foram preenchidos
 
-    //Implementa estrutura que testa se todos os campos foram preenhcidos
     if(!loginEmail || !loginSenha){
         alert("Favor preencher todos os campos");
-    } else{
+    }else{
         window.location.href = "cadastro.html";
     }
 }
 
-//cria variável do tipo array
+// Cria variável do tipo array
 var dadosLista = [];
 
 function salvarUser(){
@@ -21,29 +21,35 @@ function salvarUser(){
         dadosLista.push(nomeUser);
         console.log(dadosLista);
         criaLista();
-        document.getElementById('nomeUser').value ="";
-    } else{
+        document.getElementById ('nomeUser').value ="";
+    }else {
         alert("Favor informar um nome para cadastro");
     }
 }
-//Função para preencher a lista de cadastro
+
+// Função para preencher a lista de cadastro
+
 function criaLista(){
-    let tabela = "<tr><td>Nome</ td><td>Ações</td></tr>";
-    for(let i=0; i<= (dadosLista.length -1); i++){
-        tabela+="<tr><th>"+ dadosLista[i] + "</ td><td><button class='btn btn-warning' onclick=''>Editar</button><button class='btn btn-danger'onclick='excluir(this.parentNode.parentNode.rowIndex)'>Excluir</button></td></tr>";
-        document.getElementById('tabela').innerHTM = tabela;
+    let tabela = "<tr> <th>Nome</th><th></th></tr>";
+    for(let i = 0; i <= (dadosLista.length -1); i ++){
+        tabela += "<tr><td>" + dadosLista [i] + "</td><td> <button class='btn btn-warning' onclick='editar(this.parentNode.parentNode.rowIndex)'>Editar <button class='btn btn-danger' onclick= 'excluir(this.parentNode.parentNode.rowIndex)'>Excluir</button><td></tr>";
+        document.getElementById('tabela').innerHTML = tabela;
+
     }
 }
 
 //Função para excluir o nome da lista
+
 function excluir(i){
     dadosLista.splice((i-1), 1);
     document.getElementById('tabela').deleteRow(i);
-    console.log(dadosLista);
+    
 }
 
-//Função para edição de nome da lista
+// Função para edição de nome da lista
 function editar(i){
-    document.getElementById('nomeUser').value = dadosLista[(i - 1)];
-    dadosLista.splice(dadosLista((i - 1)), 1);
+    document.getElementById('nomeUser').
+    value = dadosLista[(i - 1)];
+    dadosLista.splice((i - 1),1);
+    
 }
