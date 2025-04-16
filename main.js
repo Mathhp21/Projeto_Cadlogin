769eb5e7be4cacbcfc01f9c21def87cd4efe52aa
Ramonpol10 <ramonzinevil369@gmai.com>
2025-04-15 21:58:44 -0300

    finalização do codigo

--- main.js
+++ main.js
@@ -10,3 +10,40 @@ function acessar (){
         window.location.href = "cadastro.html";
     }
 }
+
+//cria variável do tipo array
+var dadosLista = [];
+
+function salvarUser(){
+    let nomeUser = document.getElementById('nomeUser').value;
+
+    if(nomeUser){
+        dadosLista.push(nomeUser);
+        console.log(dadosLista);
+        criaLista();
+        document.getElementById('nomeUser').value ="";
+    } else{
+        alert("Favor informar um nome para cadastro");
+    }
+}
+//Função para preencher a lista de cadastro
+function criaLista(){
+    let tabela = "<tr><td>Nome</ td><td>Ações</td></tr>";
+    for(let i=0; i<= (dadosLista.length -1); i++){
+        tabela+="<tr><th>"+ dadosLista[i] + "</ td><td><button class='btn btn-warning' onclick=''>Editar</button><button class='btn btn-danger'onclick='excluir(this.parentNode.parentNode.rowIndex)'>Excluir</button></td></tr>";
+        document.getElementById('tabela').innerHTM = tabela;
+    }
+}
+
+//Função para excluir o nome da lista
+function excluir(i){
+    dadosLista.splice((i-1), 1);
+    document.getElementById('tabela').deleteRow(i);
+    console.log(dadosLista);
+}
+
+//Função para edição de nome da lista
+function editar(i){
+    document.getElementById('nomeUser').value = dadosLista[(i - 1)];
+    dadosLista.splice(dadosLista((i - 1)), 1);
+}
